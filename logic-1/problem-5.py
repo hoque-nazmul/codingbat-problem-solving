44d280fd6b59171e1a45ad64d1a35c444690d603
nazmul98 <coder.nazmul@gmail.com>
2021-08-08 20:59:14 -0400

    solved login-1 all problems

---
+++ logic-1/problem-5.py
@@ -0,0 +1,11 @@
+# Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+# sorta_sum(3, 4) → 7
+# sorta_sum(9, 4) → 20
+# sorta_sum(10, 11) → 21
+
+def sorta_sum(a, b):
+  result = a + b
+  if result in range(10, 20):
+    return 20
+  return result
